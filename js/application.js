3cb9251b063080538958dc4177c48794c20c6a3b
schmooser <schmooser@gmail.com>
2013-05-09 14:15:02 +0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -2,4 +2,18 @@ $(function() {
     $('span.photos').fotorama({
        'nav': 'dots'
      });
+
+$('span.code').each(function(){
+  var url = $(this).children('a').attr('href')
+  var a = $('<a>')
+  $(a).attr('href', url).html('View source')
+  $(this).html('&nbsp;')
+  $('<iframe>', {
+   src: url,
+   frameborder: 0,
+   scrolling: 'no'
+   }).appendTo(this)
+  $(this).append($('<p>').append(a))
+})
+
 });
