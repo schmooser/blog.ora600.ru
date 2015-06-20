13a400a50f4a691fe703c7adb560348707613888
Pavel Popov <pavelpopov@outlook.com>
2015-06-20 21:57:42 +0300

    Add generate.sh and CNAME

---
+++ generate.sh
@@ -0,0 +1,22 @@
+SRC_DIR=~/Projects/ora600.ru
+TARGET_DIR=~/Projects/ora600.ru-gh-pages
+
+/bin/rm -rf $TARGET_DIR/*
+
+hugo --source=$SRC_DIR --destination=$TARGET_DIR
+
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+cd $TARGET_DIR
+
+if [ -z "$(git status --porcelain)" ]; then
+    # working directory clean
+    exit 0
+else
+    # uncommited changes
+    git add --all .
+    git commit -m "Site generated at `date +\"%Y-%m-%d %H:%M:%S\"`"
+    git push origin gh-pages
+fi
